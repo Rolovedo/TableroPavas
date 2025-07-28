9586b098286fd6b867c1586b2516628123d1f5b2
Rolovedo <acevedo314848@gmail.com>
2025-07-27 19:18:37 -0500

    Limpieza completa de archivos obsoletos

--- server/src/modules/app/whatsapp/whatsapp.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { receiveWhatsappResponse } from "./whatsapp.controller.js";
-
-const whatsappRoutes = express.Router();
-
-whatsappRoutes.post("/webhook", receiveWhatsappResponse);
-
-export default whatsappRoutes;
